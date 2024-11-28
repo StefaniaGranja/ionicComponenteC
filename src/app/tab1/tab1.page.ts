d1442f6f842cb411644cadc0856f2e3a235de03a
carolinanazareno2594 <carolina20@ymail.com>
2024-11-27 22:37:24 -0500

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  saludo: string = '';
+  constructor() {
+    this.saludo = 'Hola a todos, biembenidos a mi primer APP';
+  }
 }
